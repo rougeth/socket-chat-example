d3a293b8e5823c9aa727a09e4fe992e56ecec994
Marco Rougeth <marco@rougeth.com>
2014-06-21 18:38:48 -0300

    Update example

--- chat.js
+++ chat.js
@@ -1,8 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(request, response) {
-    response.send('<h1>Hello World</h1>');
+    response.sendfile('chat.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('New user connected');
+    socket.on('disconnect', function() {
+        console.log('User disconnected');
+    });
+    socket.on('new message', function(msg) {
+        io.emit('new message', msg);
+    });
 });
 
 http.listen(3000, function() {
