f35750d770c97937b7b0328dff1663da73163f5c
Marco Rougeth <marco@rougeth.com>
2014-06-23 01:48:43 -0300

    Updating chat settings

--- src/chat.js
+++ src/chat.js
@@ -1,11 +1,54 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(handler);
+var url = require('url');
+var fs = require('fs');
 var io = require('socket.io')(http);
 
-app.get('/', function(request, response) {
-    response.sendfile('chat.html');
+
+http.listen(3000);
+
+
+function handler (request, response) {
+    console.log(request.headers['host'] + ': ' + request.method + ' ' + request.url);
+
+    var path = url.parse(request.url).pathname;
+
+    switch(path) {
+        case '/':
+            fs.readFile('./chat.html', function (error, data) {
+                response.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                response.write(data, 'utf-8');
+                response.end();
+            });
+            break;
+        default:
+            response.writeHead(404);
+            response.write('This page does not exists!');
+            response.end();
+            break;
+    }
+};
+
+
+io.on('connection', function(socket) {
+    socket.broadcast.emit('chat-new-user', 'new user connected');
+
+    socket.on('chat-message', function(msg) {
+        console.log(socket.username + ': ' + msg);
+        io.emit('chat-message', {
+            'user': socket.username,
+            'message': msg
+        });
+    });
+    socket.on('set-username', function(username) {
+        socket.username = username;
+    });
 });
 
+
+
+/*
 io.on('connection', function(socket) {
     console.log('New user connected');
     socket.on('disconnect', function() {
@@ -20,3 +63,4 @@ io.on('connection', function(socket) {
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
+*/
